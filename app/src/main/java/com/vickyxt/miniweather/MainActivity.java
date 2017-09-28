312d6cd98e5e39c5dc6f8fb4258601af9ec440ec
VickyXT <vickyxt@163.com>
2017-09-28 16:18:31 +0800

    制作检查网络状态工具类

--- app/src/main/java/com/vickyxt/miniweather/MainActivity.java
+++ app/src/main/java/com/vickyxt/miniweather/MainActivity.java
@@ -2,6 +2,10 @@ package com.vickyxt.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.vickyxt.util.NetUtil;
 
 /**
  * Created by VickyXT on 2017/9/21.
@@ -12,5 +16,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.weather_info);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("myWeather","网络ok");
+            Toast.makeText(MainActivity.this,"网络ok！",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Log.d("myWeather","网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了",Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/com/vickyxt/util/NetUtil.java
@@ -0,0 +1,33 @@
+package com.vickyxt.util;
+
+/**
+ * Created by VickyXT on 2017/9/28.
+ */
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+public class NetUtil {
+    public static final int NETWORN_NONE = 0;
+    public static final int NETWORN_WIFI = 1;
+    public static final int NETWORN_MOBILE = 2;
+
+    public static int getNetworkState(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo == null){
+            return NETWORN_NONE;
+        }
+
+        int nType = networkInfo.getType();
+        if (nType == ConnectivityManager.TYPE_MOBILE){
+            return NETWORN_MOBILE;
+        }else if (nType == ConnectivityManager.TYPE_WIFI){
+            return NETWORN_WIFI;
+        }
+
+        return NETWORN_NONE;
+    }
+}
