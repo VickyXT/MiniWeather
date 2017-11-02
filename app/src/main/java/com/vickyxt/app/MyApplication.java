2d80d476ea7ed6999053ac41612bfc0a66ffc95c
VickyXT <vickyxt@163.com>
2017-11-02 18:57:23 +0800

    初始化城市信息

--- app/src/main/java/com/vickyxt/app/MyApplication.java
+++ app/src/main/java/com/vickyxt/app/MyApplication.java
@@ -4,9 +4,15 @@ import android.app.Application;
 import android.os.Environment;
 import android.util.Log;
 
+import com.vickyxt.bean.City;
 import com.vickyxt.db.CityDB;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by VickyXT on 2017/11/1.
@@ -16,6 +22,9 @@ public class MyApplication extends Application {
     private static final String TAG = "MyAPP";
 
     private static MyApplication myApplication;
+    private CityDB mCityDB;
+
+    private List<City> mCityList;
 
     @Override
     public void onCreate(){
@@ -23,6 +32,36 @@ public class MyApplication extends Application {
         Log.d(TAG,"MyApplication->Oncreate");
 
         myApplication = this;
+        mCityDB = openCityDB();
+        initCityList();
+    }
+
+    private void initCityList(){
+        mCityList = new ArrayList<City>();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //ToDo Auto-generated method stub
+                prepareCityList();
+            }
+        }).start();
+    }
+
+    private boolean prepareCityList() {
+        mCityList = mCityDB.getAllCity();
+        int i = 0;
+        for (City city : mCityList){
+            i++;
+            String cityName = city.getCity();
+            String cityCode = city.getNumber();
+            Log.d(TAG,cityCode+":"+cityName);
+        }
+        Log.d(TAG,"i="+i);
+        return true;
+    }
+
+    public List<City> getCityList(){
+        return mCityList;
     }
 
     public static MyApplication getInstance(){
@@ -38,5 +77,35 @@ public class MyApplication extends Application {
                 +CityDB.CITY_DB_NAME;
         File db = new File(path);
         Log.d(TAG,path);
+        if (!db.exists()){
+
+            String pathfolder = "/data"
+                    + Environment.getDataDirectory().getAbsolutePath()
+                    + File.separator + getPackageName()
+                    + File.separator + "database1"
+                    + File.separator;
+            File dirFirstFolder = new File(pathfolder);
+            if (!dirFirstFolder.exists()){
+                dirFirstFolder.mkdirs();
+                Log.i("MyApp","mkdirs");
+            }
+            Log.i("MyApp","db is not exists");
+            try {
+                InputStream is = getAssets().open("city.db");
+                FileOutputStream fos = new FileOutputStream(db);
+                int len = -1;
+                byte[] buffer = new byte[1024];
+                while ((len = is.read(buffer)) != -1){
+                    fos.write(buffer,0,len);
+                    fos.flush();
+                }
+                fos.close();
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+        return new CityDB(this, path);
     }
 }
