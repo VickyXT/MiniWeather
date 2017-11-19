f7fe02526d75f46ede20594cf5498de4a5eeff72
VickyXT <vickyxt@163.com>
2017-11-19 20:24:11 +0800

    unfinished EditText

--- app/src/main/java/com/vickyxt/miniweather/ClearEditText.java
+++ app/src/main/java/com/vickyxt/miniweather/ClearEditText.java
@@ -2,6 +2,7 @@ package com.vickyxt.miniweather;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -12,7 +13,7 @@ import android.widget.EditText;
  * Created by vickyxt on 2017/11/19.
  */
 
-public class ClearEditText extends EditText implements View.OnFocusChangeListener, TextWatcher{
+public class ClearEditText extends android.support.v7.widget.AppCompatEditText implements View.OnFocusChangeListener, TextWatcher{
     private Drawable mClearDrawable;
     public ClearEditText(Context context, AttributeSet attributeSet){
         this(context, attributeSet,android.R.attr.editTextStyle);
@@ -26,19 +27,60 @@ public class ClearEditText extends EditText implements View.OnFocusChangeListene
     private void init(){
         mClearDrawable = getCompoundDrawables()[2];
         if (mClearDrawable == null){
-            mClearDrawable = getResources().getDrawable(R.drawable.btn);
+//            mClearDrawable = getResources().getDrawable(R.drawable.btn);
         }
         mClearDrawable.setBounds(0,0,mClearDrawable.getIntrinsicWidth(),mClearDrawable.getIntrinsicHeight());
-        setClear;
+        setClearIconVisible(false);
         setOnFocusChangeListener(this);
         addTextChangedListener(this);
     }
 
+    @Override
     public boolean onTouchEvent(MotionEvent event){
         if (getCompoundDrawables()[2]!=null){
             if (event.getAction() == MotionEvent.ACTION_UP){
-
+                boolean touchable = event.getX() > (getWidth() - getPaddingRight() - mClearDrawable.getIntrinsicWidth())&&(event.getX() < ((getWidth() - getPaddingRight())));
+                if (touchable){
+                    this.setText("");
+                }
             }
         }
+
+        return super.onTouchEvent(event);
     }
+
+    @Override
+    public void onFocusChange(View view,boolean hasFocus){
+        if (hasFocus){
+            setClearIconVisible(getText().length()>0);
+        }
+        else {
+            setClearIconVisible(false);
+        }
+    }
+
+    protected void setClearIconVisible(boolean visible){
+        Drawable right = visible?mClearDrawable:null;
+        setCompoundDrawables(getCompoundDrawables()[0],getCompoundDrawables()[1],right,getCompoundDrawables()[3]);
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s,int start,int count,int after){
+        setClearIconVisible(s.length()>0);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s,int start,int count,int after){
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s){
+
+    }
+
+//    晃动动画
+//    public void setShakeAnimation(){
+//        this.setAnimation(shakeAnimation(5));
+//    }
 }
--- app/src/main/java/com/vickyxt/miniweather/SelectCity.java
+++ app/src/main/java/com/vickyxt/miniweather/SelectCity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -30,12 +33,35 @@ public class SelectCity extends Activity implements View.OnClickListener{
     private ImageView mBackBtn;
     private ListView mListView;
     private List<City> mCityList;
+    private ClearEditText mClearEditText;
+    private ArrayList<City> filterDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.select_city);
+        mClearEditText = (ClearEditText) findViewById(R.id.search_city);
+        //根据输入框输入值的改变来过滤搜索
+        mClearEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //当输入框内值为空，更新为原来列表，否则为过滤数据列表
+                filterData(s.toString());
+                mListView.setAdapter();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
 
         initCityView();
@@ -89,4 +115,26 @@ public class SelectCity extends Activity implements View.OnClickListener{
             }
         });
     }
+
+    //根据输入框中的值过滤数据并更新mListView
+    private void filterData(String filterStr){
+        filterDataList = new ArrayList<City>();
+        Log.d("Filter",filterStr);
+
+        if (TextUtils.isEmpty(filterStr)){
+            for (City city:mCityList){
+                filterDataList.add(city);
+            }
+        }
+        else {
+            filterDataList.clear();
+            for (City city:mCityList){
+                if (city.getCity().indexOf(filterStr.toString() != -1)){
+                    filterDataList.add(city);
+                }
+            }
+        }
+    }
+    //根据a-z排序
+
 }
